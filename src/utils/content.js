566fe2ccb65dd99e6c2dd5fd03a189b2d880e887
Jai Balaji <77879736+jaibalaji2509@users.noreply.github.com>
2022-08-19 14:54:50 +0530

    Update content.js

--- src/utils/content.js
+++ src/utils/content.js
@@ -1,24 +1,21 @@
 export const accordionData = [
     {
-      title: 'Section 1',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
+      title: 'InterSchool Football Competition',
+      content: `Those born on or after January 1, 2009 will be eligible to take part in Under-15 age category while those born on or after January 1, 2006 will take part in Under-15 age category. The national championship is scheduled to be held from September 1 to October 17, adds the release..`
     },
     {
-      title: 'Section 2',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-      Repudiandae, mollitia id reprehenderit a ab odit!`
+      title: 'InterSchool Speech Competition',
+      content: `Make eye contact with your audience.
+      Start from the conclusion.
+      Recite a quote or a poetry verse.
+      Open up and say something about yourself.
+      Use a narrative tool to start your speech.
+      Make a joke or two.
+      Say something not widely known.
+      Engage your audience with a thought-provoking question.`
     },
     {
-      title: 'Section 3',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
+      title: 'InterSchool Chess Competition',
+      content: `Today, the most recognized chess tournaments for individual competition include the Linares chess tournament (now defunct) and the Tata Steel chess tournament. The largest team chess tournament is the Chess Olympiad, in which players compete for their country's team in the same fashion as the Olympic Games.`
     }
   ];
